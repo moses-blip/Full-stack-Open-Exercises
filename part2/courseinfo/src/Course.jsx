f1d7019564962ca2ea8c38c3c29713bc29632c42
Mosses <mosesfikirini97@gmail.com>
2025-09-30 14:38:57 +0300

    Added total components in step 7

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -21,7 +21,12 @@ const Course = ({ course }) => (
   <div>
     <Header course={course} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 );
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p><strong>{total} exercises</strong></p>;
+};
 
 export default Course;
